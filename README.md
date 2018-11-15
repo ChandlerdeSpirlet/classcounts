0f4ffa7d7121b5c8889c4df974425d0ffce82140
Chandler de Spirlet <chandler.despirlet@icloud.com>
2018-11-14 19:30:15 -0700

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# classcounts
+Database integrated webpage that tracks the number of classes per black belt.
+## Expected release date - 10 January 2019
---
+++ routes/store.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var db = require('../database');
+var app = express();
+module.exports = app;
+app.get('/', function (request, response) {
+
+    // TODO: Initialize the query variable with a SQL query
+    // that returns all the rows and columns in the 'store' table
+    var query = 'SELECT * FROM counts';
+
+    db.any(query)
+        .then(function (rows) {
+        // render views/store/list.ejs template file
+        response.render('store/list', {
+            title: 'Class Counts',
+            data: rows
+        })
+    })
+    .catch(function (err) {
+        // display error message in case an error
+        request.flash('error', err);
+        response.render('store/list', {
+            title: 'Class Counts',
+            data: ''
+        })
+    })
+});
