d2b12156f856e989d5f7de91e894d55c67e49932
Chandler de Spirlet <chandler.despirlet@icloud.com>
2018-11-14 20:12:53 -0700

    BETA 1.1
    Attempting to add form to add black belt.

--- routes/store.js
+++ routes/store.js
@@ -25,3 +25,46 @@ app.get('/', function (request, response) {
         })
     })
 });
+app.post('/add', function (request, response) {
+    // Validate user input - ensure non emptiness
+    request.assert('barcode', 'Barcode is required').notEmpty();
+    request.assert('bbname', 'Name is required').notEmpty();
+
+    var errors = request.validationErrors();
+    if (!errors) { // No validation errors
+        var item = {
+            // sanitize() is a function used to prevent Hackers from inserting
+            // malicious code(as data) into our database. There by preventing
+            // SQL-injection attacks.
+            barcode: request.sanitize('barcode').escape().trim(),
+            bbname: request.sanitize('bbname').escape().trim()
+        };
+        // Running SQL query to insert data into the store table
+        db.none('INSERT INTO counts(barcode, bbname) VALUES($1, $2)', [item.barcode, item.bbname])
+            .then(function (result) {
+                request.flash('success', 'Data added successfully!');
+                // render views/store/add.ejs
+                response.render('store/add', {
+                    title: 'Add New Blackbelt',
+                    barcode: '',
+                    bbname: ''
+                })
+            }).catch(function (err) {
+            request.flash('error', err);
+            // render views/store/add.ejs
+            response.render('store/add', {
+                title: 'Add New Blackbelt',
+                barcode: item.barcode,
+                bbname: item.bbname
+            })
+        })
+    } else {
+        var error_msg = errors.reduce((accumulator, current_error) => accumulator + '<br />' + current_error.msg, '');
+        request.flash('error', error_msg);
+        response.render('store/add', {
+            title: 'Add New Blackbelt',
+            barcode: request.body.barcode,
+            bbname: request.body.bbname
+        })
+    }
+});
