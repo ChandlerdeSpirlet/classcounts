5522914f36a55248647e15dcb7bd1a05fbfbe699
Chandler de Spirlet <chandler.despirlet@icloud.com>
2018-11-15 15:38:30 -0700

    BETA 1
    Adds delete button and refresh database query

--- routes/store.js
+++ routes/store.js
@@ -84,3 +84,15 @@ app.post('/add', function (request, response) {
         })
     }
 });
+app.delete('/delete', function (req, res) {
+    var deleteQuery = 'update counts set regular = 0, sparring = 0, swats = 0 where barcode > 0';
+    db.none(deleteQuery)
+        .then(function (result) {
+                    req.flash('success', 'Successfully refreshed classes');
+                    res.redirect('/store');
+        })
+        .catch(function (err) {
+                    req.flash('error', err);
+                    res.redirect('/store')
+        })
+});
