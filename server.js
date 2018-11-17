5dd25dda609d0dc856a79be0e71161ebea70fe8f
Chandler de Spirlet <chandler.despirlet@icloud.com>
2018-11-17 14:23:02 -0700

    File upload

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ var index = require('./routes/index');
 var store = require('./routes/store');
 app.use('/', index);
 app.use('/store', store);
-var port = 5000;
-//var port = process.env.PORT;
+//var port = 5000;
+var port = process.env.PORT;
 app.listen(port, function() {
     console.log('Server running on http://localhost:' + port)
 });
