1a1de15da6c95efa2befa3e32cd1fa7f177d3f8f
Chandler de Spirlet <chandler.despirlet@icloud.com>
2018-11-27 19:58:28 -0700

    Version 1.1.8.3

--- routes/store.js
+++ routes/store.js
@@ -68,6 +68,31 @@ app.get('/list2', function (request, response) {
     })
     
 });
+app.get('/list3', function (request, response) {
+    
+    // TODO: Initialize the query variable with a SQL query
+    // that returns all the rows and columns in the 'store' table
+    
+    var query = 'SELECT * FROM counts order by bbname';
+
+    db.any(query)
+        .then(function (rows) {
+        // render views/store/list.ejs template file
+        response.render('store/list3', {
+            title: 'Class Counts - Updated ' + globalDate,
+            data: rows
+        })
+    })
+    .catch(function (err) {
+        // display error message in case an error
+        request.flash('error', err);
+        response.render('store/list3', {
+            title: 'Class Counts - Updated ' + globalDate,
+            data: ''
+        })
+    })
+    
+});
 
 app.get('/file', function (request, response) {
     // render the views/index.ejs template file
