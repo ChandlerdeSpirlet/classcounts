8ebba713c06df8f6e81e54e7d83ec6178e06f9a2
Chandler de Spirlet <chandler.despirlet@icloud.com>
2019-03-02 00:35:17 -0700

    Version 3.9.6
    Adds the file hosting feature for the app.

--- routes/store.js
+++ routes/store.js
@@ -63,8 +63,88 @@ app.get('/', function (request, response) {
     })
     
 });
-app.get('/calendar', function (req, res){
-    var data =fs.readFileSync(__dirname + '/sched.pdf');
+app.get('/schedule', function (req, res){
+    var data =fs.readFileSync(__dirname + '/storedFiles/sched.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/ITP', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/ITP.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/aspHomework', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/ASPhomework.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/Lvl1Homework', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/Lvl1Homework.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/Lvl2Homework', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/Lvl2Homework.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/Lvl3Homework', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/Lvl3Homework.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/BBHomework', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/BBHomework.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/Lvl1Sparring', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/Lvl1Sparring.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/Lvl2Sparring', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/Lvl2Sparring.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/ASPPacket', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/ASPPacket.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/Calendar', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/Calendar.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/1Confidence', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/1Confidence.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/2Discipline', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/2Discipline.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/3Respect', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/3Respect.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/4Responsibility', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/4Responsibility.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/5Focus', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/5Focus.pdf');
+    res.contentType("application/pdf");
+    res.send(data);
+});
+app.get('/6GoalSetting', function(req, res){
+    var data = fs.readFileSync(__dirname + '/storedFiles/6GoalSetting.pdf');
     res.contentType("application/pdf");
     res.send(data);
 });
