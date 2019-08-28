6a0d6c5424c795a47a73add4e85d777f9e8862ac
Chandler de Spirlet <chandler.despirlet@icloud.com>
2019-08-28 14:06:14 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,15 @@ db.any(query)
         global.globalDate = temp.refreshed;
 });
 
-app.get('/', function (request, response) {
 
+
+app.get('/', function (request, response) {
+    var http = require("http");
+    setInterval(function() {
+        http.get("http://classcounts.herokuapp.com");
+    }, 300000); // every 5 minutes (300000)
+    
+    setInterval();
     // TODO: Initialize the query variable with a SQL query
     // that returns all the rows and columns in the 'store' table
     var query = 'SELECT * FROM counts order by bbname';
