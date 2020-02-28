cc65377542f3082b7adc4787fafc58bd05f66526
Chandler de Spirlet <chandler.despirlet@icloud.com>
2020-02-28 10:54:44 -0700

    Adding https redirect

--- routes/store.js
+++ routes/store.js
@@ -74,11 +74,11 @@ function getDate() {
 }
 
 app.get('/', function (request, response) {
-    //if(request.headers['x-forwarded-proto']!='https'){
-    //    response.redirect('https://classcounts.herokuapp.com')
-    //} else {
+    if(request.headers['x-forwarded-proto']!='https'){
+        response.redirect('https://classcounts.herokuapp.com')
+    } else {
         var query = 'SELECT * FROM counts order by bbname_last';
-    //var query = 'select Z.*, S.mon, S.tues, S.wed, S.thurs, S.fri from counts Z, signup S where Z.bbname like S.bbname';
+        //var query = 'select Z.*, S.mon, S.tues, S.wed, S.thurs, S.fri from counts Z, signup S where Z.bbname like S.bbname';
         getDate();
         db.any(query)
             .then(function (rows) {
@@ -98,7 +98,7 @@ app.get('/', function (request, response) {
                 data: ''
             })
         })
-    //}
+    }
     // TODO: Initialize the query variable with a SQL query
     // that returns all the rows and columns in the 'store' table
     
